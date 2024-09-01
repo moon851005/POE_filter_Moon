5ddf94c4f6fa3a6d305363d3dbe070991701f6c5
moon851005 <lelouch80548@gmail.com>
2024-09-01 13:10:24 +0800

    1. 速刷版更新 2. 新增印鈔版 3. 新增速刷、印鈔一鍵更新腳本

---
+++ AutoUpdateFilter.ps1
@@ -0,0 +1,10 @@
+# 讀取原檔案內容
+$content = Get-Content "Moon.filter"
+
+# 替換內容
+$content1 = $content -replace 'Show #印鈔 - 速刷', 'Hide #印鈔 - 速刷'
+$content2 = $content -replace 'Show #印鈔', 'Hide #印鈔'
+
+# 將修改後的內容儲存為新檔案
+$content1 | Set-Content "Moon_速刷.filter"
+$content2 | Set-Content "Moon_印鈔.filter"
