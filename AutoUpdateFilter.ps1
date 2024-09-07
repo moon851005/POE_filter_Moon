4900e2270da95b72317f9dca3626d6ec646928f2
moon851005 <lelouch80548@gmail.com>
2024-09-07 22:41:01 +0800

    檔案名稱新增編號，以提示顯示階級

--- AutoUpdateFilter.ps1
+++ AutoUpdateFilter.ps1
@@ -7,6 +7,6 @@ $content2 = $content -replace 'Show #印鈔 - 終局', 'Hide #印鈔 - 終局'
 $content3 = $content -replace 'Show #印鈔', 'Hide #印鈔'
 
 # 將修改後的內容儲存為新檔案
-$content1 | Set-Content "Moon_速刷.filter"
-$content2 | Set-Content "Moon_終局.filter"
-$content3 | Set-Content "Moon_印鈔.filter"
+$content1 | Set-Content "Moon_1_速刷.filter"
+$content2 | Set-Content "Moon_2_終局.filter"
+$content3 | Set-Content "Moon_3_印鈔.filter"
