12c56e545fb6396e06216b017de6e2d6237a5c37
moon851005 <lelouch80548@gmail.com>
2024-09-07 12:45:36 +0800

    1. 現在傳奇可以過濾輿圖中掉落(如T16)與非輿圖掉落(如巔峰頭目)，現在印鈔不會顯示低稀有傳奇 2. 新增終局版，目前繼承原印鈔的過濾，只差在仍會顯示低稀有傳奇

--- AutoUpdateFilter.ps1
+++ AutoUpdateFilter.ps1
@@ -2,9 +2,11 @@
 $content = Get-Content "Moon.filter"
 
 # 替換內容
-$content1 = $content -replace 'Show #印鈔 - 速刷', 'Hide #印鈔 - 速刷'
-$content2 = $content -replace 'Show #印鈔', 'Hide #印鈔'
+$content1 = $content -replace 'Show #印鈔 - 終局 - 速刷', 'Hide #印鈔 - 終局 - 速刷'
+$content2 = $content -replace 'Show #印鈔 - 終局', 'Hide #印鈔 - 終局'
+$content3 = $content -replace 'Show #印鈔', 'Hide #印鈔'
 
 # 將修改後的內容儲存為新檔案
 $content1 | Set-Content "Moon_速刷.filter"
-$content2 | Set-Content "Moon_印鈔.filter"
+$content2 | Set-Content "Moon_終局.filter"
+$content3 | Set-Content "Moon_印鈔.filter"
